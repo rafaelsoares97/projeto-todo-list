a8012077741ea374531b1d53d9363d06a69d99d6
Roger Santos <roger@refatorando.com.br>
2024-12-09 13:35:45 -0700

    js

---
+++ public/js/scripts.js
@@ -0,0 +1,9 @@
+// Seleciona os elementos necessários
+const toggleBtn = document.querySelector('.toggle');
+const body = document.body;
+
+// Adiciona o evento de clique no botão de toggle
+toggleBtn.addEventListener('click', () => {
+    // Alterna a classe dark-mode no body
+    body.classList.toggle('dark-mode');
+});
